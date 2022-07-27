1fe4a2155bf68d8ef932b69ed008328eec23aefd
komsanong <43783522+komsanong@users.noreply.github.com>
2022-07-27 04:45:18 +0100

    Add files via upload

--- A star.py
+++ A star.py
@@ -3,6 +3,7 @@ import pygame as pg
 import math
 import numpy as np
 from queue import PriorityQueue
+import time
 
 WIDTH = 600 
 WIN = pg.display.set_mode((WIDTH,WIDTH)) # Screen Dimension
@@ -21,6 +22,7 @@ GREY = (128,128,128)
 L_GREY = (200,200,200)
 TURQUOISE = (64,224,208)
 
+t1=0
         
 class Spot:
     def __init__(self,row,col,width,total_rows):
@@ -275,15 +277,28 @@ def get_clicked_pos(pos, rows, width):
     return row, col
 
 class RRT:
-    def __init__(self,start,end,width,height,rectang):
+    def __init__(self,start,end, width, height, rectang):
         self.start = start
         self.end = end
+        self.start_x,self.start_y = start.center
+        self.goal_x,self.goal_y = end.center
         self.width = width
         self.height = height
         self.x = []
         self.y = []
         self.parent = []
         self.rectang_list = rectang
+        
+        # initialize the tree
+        self.x.append(self.start_x)
+        self.y.append(self.start_y)
+        self.parent.append(0)
+
+        # path
+        self.goalFlag = False
+        self.goalstate = None
+        self.path = []
+        
         
     def add_node(self,n,x,y):
         self.x.insert(n,x)
@@ -301,24 +316,177 @@ class RRT:
         y = int(np.random.uniform(0,self.height))
         return x,y
 
-    def isFree(self):
-        n = self.number_of_node() - 1
-        (x,y) = (self.x[n],self.y[n])
-        while len(self.rectang_list)> 0:
-            rectang = self.rectang_list.pop(0)
+    def isFree(self,n,x,y):
+        for rectang in self.rectang_list:
             if rectang.collidepoint(x, y):
                 self.remove_node(n)
+                return False
         return True
+    
+    def add_edge(self, parent, child):
+        self.parent.insert(child, parent)
+
+    def remove_edge(self, n):
+        self.parent.pop(n)
+        
+    def distance(self, n1, n2):
+        (x1, y1) = (self.x[n1], self.y[n1])
+        (x2, y2) = (self.x[n2], self.y[n2])
+        px = (float(x1) - float(x2)) ** 2
+        py = (float(y1) - float(y2)) ** 2
+        return (px + py) ** (0.5)
+    
+    def nearest(self, n):
+        dmin = self.distance(0, n)
+        nnear = 0
+        for i in range(0, n):
+            if self.distance(i, n) < dmin:
+                dmin = self.distance(i, n)
+                nnear = i
+        return nnear
+    
+    def crossObstacle(self, x1, x2, y1, y2):
+        for rectang in self.rectang_list:
+            for i in range(0, 101):
+                u = i / 100
+                x = x1 * u + x2 * (1 - u)
+                y = y1 * u + y2 * (1 - u)
+                if rectang.collidepoint(x, y):
+                    return True
+        return False
+    
+    def connect(self, n1, n2):
+        (x1, y1) = (self.x[n1], self.y[n1])
+        (x2, y2) = (self.x[n2], self.y[n2])
+        if self.crossObstacle(x1, x2, y1, y2):
+            self.remove_node(n2)
+            return False
+        else:
+            self.add_edge(n1, n2)
+            return True
+        
+    def bias(self):
+        n = self.number_of_node()
+        self.add_node(n,self.goal_x, self.goal_y)
+        nnear = self.nearest(n)
+        self.step(nnear, n)
+        self.connect(nnear, n)
+        return self.x, self.y, self.parent
+
+    def expand(self):
+        n = self.number_of_node()
+        x, y = self.sample_envir()
+        self.add_node(n, x, y)
+        if self.isFree(n,x,y):
+            xnearest = self.nearest(n)
+            self.step(xnearest, n)
+            self.connect(xnearest, n)
+        return self.x, self.y, self.parent
+
+    def path_to_goal(self):
+        if self.goalFlag:
+            self.path = []
+            self.path.append(self.goalstate)
+            newpos = self.parent[self.goalstate]
+            while (newpos != 0):
+                self.path.append(newpos)
+                newpos = self.parent[newpos]
+            self.path.append(0)
+        return self.goalFlag
+
+    def getPathCoords(self):
+        pathCoords = []
+        for node in self.path:
+            x, y = (self.x[node], self.y[node])
+            pathCoords.append((x, y))
+        return pathCoords
+
+    def cost(self, n):
+        ninit = 0
+        n = n
+        parent = self.parent[n]
+        c = 0
+        while n is not ninit:
+            c = c + self.distance(n, parent)
+            n = parent
+            if n is not ninit:
+                parent = self.parent[n]
+        return c
+
+    def getTrueObs(self, obs):
+        TOBS = []
+        for ob in obs:
+            TOBS.append(ob.inflate(-50, -50))
+        return TOBS
+
+    def waypoints2path(self):
+        oldpath = self.getPathCoords()
+        path = []
+        for i in range(0, len(self.path) - 1):
+            print(i)
+            if i >= len(self.path):
+                break
+            x1, y1 = oldpath[i]
+            x2, y2 = oldpath[i + 1]
+            print('---------')
+            print((x1, y1), (x2, y2))
+            for i in range(0, 5):
+                u = i / 5
+                x = int(x2 * u + x1 * (1 - u))
+                y = int(y2 * u + y1 * (1 - u))
+                path.append((x, y))
+                print((x, y))
+
+        return path
+    
+    def step(self, nnear, nrand, dmax= 20, dgoal = 15):
+        d = self.distance(nnear, nrand)
+        if d > dmax:
+            u = dmax / d
+            (xnear, ynear) = (self.x[nnear], self.y[nnear])
+            (xrand, yrand) = (self.x[nrand], self.y[nrand])
+            (px, py) = (xrand - xnear, yrand - ynear)
+            theta = math.atan2(py, px)
+            (x, y) = (int(xnear + dmax * math.cos(theta)),
+                      int(ynear + dmax * math.sin(theta)))
+            self.remove_node(nrand)
+            if abs(x - self.goal_x) <= dgoal and abs(y - self.goal_y) <= dgoal:
+                self.add_node(nrand, self.goal_x, self.goal_y)
+                self.goalstate = nrand
+                self.goalFlag = True
+            else:
+                self.add_node(nrand, x, y)
+                
+    def drawPath(self,path):
+        for node in path:
+            pg.draw.circle(WIN,RED,node,3,0)
     
 def rrt_algorithm(start,end,width,height,obstacle):
     graph = RRT(start,end,width,height,obstacle)
-    while(True):
-        x,y = graph.sample_envir()
-        n = graph.number_of_node()
-        graph.add_node(n,x,y)
-        if (graph.isFree()):
-            pg.draw.circle(WIN,YELLOW,(graph.x[n-1], graph.y[n-1]),2)
-        pg.display.update()
+    iteration = 0
+    t1=time.time()
+    while(not graph.path_to_goal()):
+        elapsed=time.time()-t1
+        t1=time.time()
+        if elapsed > 10:
+            raise
+        if iteration % 2 == 0:
+            x,y,Parent = graph.bias()
+            pg.draw.circle(WIN,GREY,(x[-1],y[-1]),2,0)
+            pg.draw.line(WIN,BLUE,(x[-1],y[-1]),(x[Parent[-1]],y[Parent[-1]]),2)
+        
+        else:
+            x,y,Parent = graph.expand()
+            pg.draw.circle(WIN,GREY,(x[-1],y[-1]),2,0)
+            pg.draw.line(WIN,BLUE,(x[-1],y[-1]),(x[Parent[-1]],y[Parent[-1]]),2)
+            
+        if iteration % 5 == 0:
+            pg.display.update()
+        iteration +=1
+    graph.drawPath(graph.getPathCoords())   
+    pg.display.update()
+    pg.event.clear()
+    pg.event.wait()
     
 
 def main(win, width):
@@ -345,20 +513,15 @@ def main(win, width):
                 node = grid[row][col]
                 if not start and node != end:
                     start = node
-                    a = start.start_rect()
-                    rectang_list.append(a)
                     start.make_start()
                     
                 elif not end and node != start:
                     end = node
-                    b = end.end_rect()
-                    rectang_list.append(b)
                     end.make_end()
                     
                 elif node != end and node != start:
                     obstacles = node
-                    c = obstacles.obs_rect()
-                    rectang_list.append(c)
+                    rectang_list.append(obstacles.obs_rect())
                     obstacles.make_barrier()
                     
             elif pg.mouse.get_pressed()[2]: # RIGHT
@@ -389,7 +552,7 @@ def main(win, width):
                     A_star_clear (lambda: draw(win, grid, ROWS, width), grid, start, end)
                     
                 if event.key == pg.K_r:                            
-                    rrt_algorithm(start,end,width,width,a)
+                    rrt_algorithm(start.rect,end.rect,width,width,rectang_list)
                   
     pg.quit()
 
